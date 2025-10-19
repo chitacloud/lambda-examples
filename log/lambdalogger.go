b986066a6f60467e342a3c2519554ee982359cf0
jmagdalena <jhon.magdalena@chitacloud.com>
2025-10-19 18:00:09 +0200

    refactor: restructure Log function to use structured LogRequest instead of generic map

--- log/lambdalogger.go
+++ log/lambdalogger.go
@@ -2,8 +2,15 @@ package logger
 
 import "fmt"
 
-func Log(params map[string]any) {
-	for key, value := range params {
-		fmt.Printf("%s: %v\n", key, value)
-	}
+type LogRequest struct {
+	Level     string `json:"l"`
+	Message   string `json:"m"`
+	Timestamp string `json:"t"`
+	Service   string `json:"s"`
+	RequestID string `json:"rid"`
+}
+
+func Log(request LogRequest) {
+	fmt.Printf("t=%s level=%s service=%s rid=%s msg=\"%s\"\n", 
+		request.Timestamp, request.Level, request.Service, request.RequestID, request.Message)
 }
