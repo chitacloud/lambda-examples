e403afa9bef00010162d1d40ac7622e4144e8c80
jmagdalena <jhon.magdalena@chitacloud.com>
2025-10-19 18:23:05 +0200

    feat: add session ID field to lambda logger output

--- log/lambdalogger.go
+++ log/lambdalogger.go
@@ -10,11 +10,12 @@ type LogRequest struct {
 	Message   string `json:"m"`
 	Timestamp string `json:"t"`
 	Service   string `json:"s"`
+	SessionID string `json:"sid"`
 	RequestID string `json:"rid"`
 }
 
 func Log(request LogRequest) {
 	escapedMsg := strings.ReplaceAll(request.Message, "\"", "\\\"")
-	fmt.Printf("t=%s level=%s service=%s rid=%s msg=\"%s\"\n",
-		request.Timestamp, request.Level, request.Service, request.RequestID, escapedMsg)
+	fmt.Printf("t=%s level=%s service=%s sid=%s rid=%s msg=\"%s\"\n",
+		request.Timestamp, request.Level, request.Service, request.SessionID, request.RequestID, escapedMsg)
 }
